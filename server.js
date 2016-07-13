acb198e57710d9a5c481e24e5938f578d07cfbaa
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-13 22:56:59 +0430

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 var todos = [{
-	id: '1',
+	id: 1,
 	description: 'Meet Mom',
 	completed: 'false'
 }, {
-	id: '2',
+	id: 2,
 	description: 'Buy Milk',
 	completed: 'false'	
+}, {
+	id: 3,
+	description: 'Feed Dog',
+	completed: 'true'	
 }];
 
 
@@ -24,7 +28,23 @@ app.get('/todos', function(req, res) {
 });
 
 //GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if (todo.id === todoId)
+			matchedTodo = todo;
+	});
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	// res.send('Asking for todo with id: ' + todoId);
+});
 
 app.listen(port, function() {
-	console.log('Express is listening on port' + port + '!');
+	console.log('Express is listening on port ' + port + '!');
 });
