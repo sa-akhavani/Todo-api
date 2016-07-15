0c58c9345a1797769b31ac26c34b48dab330a933
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-15 16:32:50 +0430

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -70,6 +70,37 @@ app.delete('/todos/:id', function (req, res) {
 });
 
 
+//----------------------------------------------
+//----------------------------------------------
+
+//PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;	
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;	
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+
+});
+
 app.listen(port, function() {
 	console.log('Express is listening on port ' + port + '!');
 });
