cd63456507ce690099465b2ba446d0a40e1eef36
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-15 17:16:38 +0430

    Add variable playground for passing by reference

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name: 'Ali',
+// 	age: 25
+// }
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	age: 20,
+// 	// 	name: 'ali'
+// 	// }
+// 	obj.age = 21;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [10, 12]
+function addGrade(arr, num) {
+	arr.push(num);
+	debugger;
+	// arr = [20, 47];
+}
+
+addGrade(grades, 23);
+console.log(grades);
