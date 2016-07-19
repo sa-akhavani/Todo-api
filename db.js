0f0e03af0ee0a1be01c704fba90e190449b7c9ca
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-19 16:14:23 +0430

    Fix missing '' for environmet

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 var Sequelize = require('sequelize');
-var env = process.env.NODE_ENV || development;
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
 	sequelize = new Sequelize(process.env.DATABASE_URL, {
 		'dialect': 'postgres',
-	})
+	});
 } else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
