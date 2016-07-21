3d9e79a666c749871d3cb08e8a608edfdc62420d
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-21 13:21:20 +0430

    Hash and salt password

--- server.js
+++ server.js
@@ -134,10 +134,12 @@ app.put('/todos/:id', function(req, res) {
 //--------------------------------------------------------
 //--------------------------------------------------------
 
+
+// POST /users
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}).catch(function(e) {
 		res.status(400).json(e);
 	})
