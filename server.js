016e79718bcf8ea9b55e5f5531c03e38a162d1ef
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2016-07-21 12:44:57 +0430

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -131,6 +131,19 @@ app.put('/todos/:id', function(req, res) {
 	})
 });
 
+//--------------------------------------------------------
+//--------------------------------------------------------
+
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+	db.user.create(body).then(function(user) {
+		res.json(user.toJSON());
+	}).catch(function(e) {
+		res.status(400).json(e);
+	})
+});
+
+
 
 db.sequelize.sync({
 	// force: true
